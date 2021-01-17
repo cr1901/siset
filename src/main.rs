3a2bea0f794a2fe7f3f376232e2326674947739a
William D. Jones <thor0505@comcast.net>
2021-01-17 11:24:20 -0500

    Add option to select PLL to use.

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,25 @@ use si5351::{Si5351, Si5351Device};
 #[derive(FromArgs)]
 #[argh(description = "set Adafruit Si5351 module frequency")]
 struct InputArgs {
+    /// PLL to use to set output clock.
+    #[argh(option, short='p', default = "default_pll()", from_str_fn(arg_pll))]
+    pll: si5351::PLL,
     #[argh(positional)]
     bus: String,
     #[argh(positional, from_str_fn(from_base_10))]
     freq: u32,
+}
+
+fn default_pll() -> si5351::PLL {
+    si5351::PLL::A
+}
+
+fn arg_pll(val: &str) -> Result<si5351::PLL, String> {
+    match val {
+        "A" => Ok(si5351::PLL::A),
+        "B" => Ok(si5351::PLL::B),
+        _ => Err("PLL must be \"A\" or \"B\"".into()),
+    }
 }
 
 fn from_base_10(val: &str) -> Result<u32, String> {
@@ -17,6 +32,22 @@ fn from_base_10(val: &str) -> Result<u32, String> {
         Ok(v) => Ok(v),
         Err(_) => Err("Unable to convert address from base 10".into()),
     }
+}
+
+fn print_action_taken(clk: si5351::ClockOutput, freq: u32, pll: si5351::PLL) {
+    let pll_str = match pll {
+        si5351::PLL::A => "A",
+        si5351::PLL::B => "B"
+    };
+
+    let clk_str = match clk {
+        si5351::ClockOutput::Clk0 => "0",
+        si5351::ClockOutput::Clk1 => "1",
+        si5351::ClockOutput::Clk2 => "2",
+        _ => unreachable!()
+    };
+
+    println!("Clk {} set to {} Hz using PLL {}.", clk_str, freq, pll_str);
 }
 
 fn main() -> eyre::Result<()> {
@@ -28,9 +59,9 @@ fn main() -> eyre::Result<()> {
     clock.init_adafruit_module().map_err(Report::msg)?;
 
     clock
-        .set_frequency(si5351::PLL::A, si5351::ClockOutput::Clk0, args.freq)
+        .set_frequency(args.pll, si5351::ClockOutput::Clk0, args.freq)
         .map_err(Report::msg)?;
 
-    println!("PLL frequency of clk 0 set to {} Hz.", args.freq);
+    print_action_taken(si5351::ClockOutput::Clk0, args.freq, args.pll);
     Ok(())
 }
